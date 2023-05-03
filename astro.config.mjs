f8bed92c1d83838a2b048e671d5c4a005d972c1f
v8def90 <v8def90@gmail.com>
2023-05-03 18:25:28 +0900

    feat: 1st step

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import { siteMeta } from './src/lib/constants'
+
+const { siteUrl } = siteMeta;
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: siteUrl
+});
---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const siteMeta = {
+	siteTitle: 'Tech Flow.',
+	siteDesc: 'Tech Flowの技術ブログサイト',
+	siteUrl: 'https://***********',
+	siteLocale: 'ja_JP',
+	siteType: 'website',
+	siteIcon: '/favicon.png',
+	siteImg: '/ogp.jpg',
+};
