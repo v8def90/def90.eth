0a7ac9c2e0748293d21ec2c61abfc506c81af03f
v8def90 <v8def90@gmail.com>
2023-05-04 13:04:02 +0900

    feat: astro step done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
-import { siteMeta } from './src/lib/constants'
+import { siteMeta } from './src/lib/constants';
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+const {
+  siteUrl
+} = siteMeta;
 
-const { siteUrl } = siteMeta;
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteUrl
+  site: siteUrl,
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), mdx()]
 });
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,3 +7,8 @@ export const siteMeta = {
 	siteIcon: '/favicon.png',
 	siteImg: '/ogp.jpg',
 };
+
+export const categories = [
+	{ categoryName: 'Python', categorySlug: 'python'},
+	{ categoryName: 'JavaScript', categorySlug: 'javascript'}
+];
