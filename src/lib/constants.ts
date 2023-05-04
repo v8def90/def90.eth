6750291389f0bcb6969084a91b2cb4f9da25d9ce
v8def90 <v8def90@gmail.com>
2023-05-04 13:40:19 +0900

    add: siteUrlの設定

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const siteMeta = {
 	siteTitle: 'Tech Flow.',
 	siteDesc: 'Tech Flowの技術ブログサイト',
-	siteUrl: 'https://***********',
+	siteUrl: 'https://def90eth.vercel.app/',
 	siteLocale: 'ja_JP',
 	siteType: 'website',
 	siteIcon: '/favicon.png',
