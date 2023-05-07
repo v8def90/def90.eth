094c7da5988cb4657da1858c477002593dc468e7
v8def90 <v8def90@gmail.com>
2023-05-07 17:15:46 +0900

    husky test6

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,18 @@
 import { z, defineCollection } from 'astro:content';
-import { zonedTimeToUtc } from 'date-fns-tz';
+// import { zonedTimeToUtc } from 'date-fns-tz';
 
 const blogCollection = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		// pubDate: z.string().transform((str) => zonedTimeToUtc(str, 'Asia/Tokyo')),
-		pubDate: z.coerce.date(),
-		image: z.string(),
-		category: z.array(z.string()),
-		description: z.string(),
-		draft: z.boolean()
-	}),
+  schema: z.object({
+    title: z.string(),
+    // pubDate: z.string().transform((str) => zonedTimeToUtc(str, 'Asia/Tokyo')),
+    pubDate: z.coerce.date(),
+    image: z.string(),
+    category: z.array(z.string()),
+    description: z.string(),
+    draft: z.boolean()
+  })
 });
 
 export const collections = {
-	'blog': blogCollection,
+  blog: blogCollection
 };
