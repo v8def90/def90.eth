3f77b42767ceffca0fca9a96370072cbdc044119
v8def90 <v8def90@gmail.com>
2023-05-15 21:43:08 +0900

    add: Categoryを追加

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,14 +1,20 @@
 export const siteMeta = {
-	siteTitle: 'Tech Flow.',
-	siteDesc: 'Tech Flowの技術ブログサイト',
-	siteUrl: 'https://def90eth.vercel.app/',
-	siteLocale: 'ja_JP',
-	siteType: 'website',
-	siteIcon: '/favicon.png',
-	siteImg: '/ogp.jpg',
+  siteTitle: 'Tech Flow.',
+  siteDesc: 'Tech Flowの技術ブログサイト',
+  siteUrl: 'https://def90eth.vercel.app/',
+  siteLocale: 'ja_JP',
+  siteType: 'website',
+  siteIcon: '/favicon.png',
+  siteImg: '/ogp.jpg'
 };
 
 export const categories = [
-	{ categoryName: 'Python', categorySlug: 'python'},
-	{ categoryName: 'JavaScript', categorySlug: 'javascript'}
+  { categoryName: 'ポエム', categorySlug: 'poem' },
+  { categoryName: 'JavaScript', categorySlug: 'javascript' },
+  { categoryName: 'TypeScript', categorySlug: 'typescript' },
+  { categoryName: 'NextJs', categorySlug: 'nextjs' },
+  { categoryName: 'Astro', categorySlug: 'astro' },
+  { categoryName: 'Web3', categorySlug: 'web3' },
+  { categoryName: 'AI', categorySlug: 'ai' },
+  { categoryName: 'AWS', categorySlug: 'aws' }
 ];
