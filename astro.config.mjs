d6f34c5140431b1a163c9a3f36603663f4426d58
v8def90 <109057534+v8def90@users.noreply.github.com>
2023-05-15 19:21:42 +0900

    Feat: sentry各種設定を追加 (#2)
    * sentryの設定を追加
    
    * snapshotを変更
    
    * Fix: source-mapをbuild後削除するよう変更
    
    * fix: yarn installエラー対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
-import { defineConfig } from 'astro/config';
-import { siteMeta } from './src/lib/constants';
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
+import node from "@astrojs/node";
 import prefetch from "@astrojs/prefetch";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
+import { siteMeta } from './src/lib/constants';
 const {
   siteUrl
 } = siteMeta;
+
+// eslint-disable-next-line no-undef
+const env = loadEnv("production", process.cwd());
 
 
 // https://astro.build/config
@@ -16,5 +22,29 @@ export default defineConfig({
   site: siteUrl,
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), mdx(), prefetch(), sitemap(), tailwind(), react()]
+  }), mdx(), prefetch(), sitemap(), tailwind(), react()],
+  // output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  vite: {
+    plugins: [
+    // Put the Sentry vite plugin after all other plugins
+    sentryVitePlugin({
+      org: "personal-a0p",
+      project: "def90eth",
+      // Specify the directory containing build artifacts
+      include: "./dist",
+      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+      // and needs the `project:releases` and `org:read` scopes
+      authToken: env.SENTRY_AUTH_TOKEN
+
+      // Optionally uncomment the line below to override automatic release name detection
+      // release: env.RELEASE,
+    })],
+
+    build: {
+      sourcemap: true
+    }
+  }
 });
